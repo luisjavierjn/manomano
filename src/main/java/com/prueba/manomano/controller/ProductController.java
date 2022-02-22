4d5c40d070f512c9830b620547a935b56990bb1a
Luis Jimenez <luis.jimenez@nordstrom.com>
2022-02-22 14:48:21 -0500

    step 3 ready

--- src/main/java/com/prueba/manomano/controller/ProductController.java
+++ src/main/java/com/prueba/manomano/controller/ProductController.java
@@ -2,15 +2,21 @@ package com.prueba.manomano.controller;
 
 import com.prueba.manomano.api.ProductsApi;
 import com.prueba.manomano.model.ProductResponseList;
+import io.swagger.annotations.ApiParam;
+import java.math.BigDecimal;
 import org.springframework.http.ResponseEntity;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public class ProductController implements ProductsApi {
 
     @Override
-    public ResponseEntity<ProductResponseList> getProducts(@NotNull @Valid String discountExpDate) {
+    public ResponseEntity<ProductResponseList> getProducts(
+            @ApiParam(value = "", required = true) @PathVariable("catId") String catId,
+            @ApiParam(value = "", required = true) @PathVariable("minPrice") BigDecimal minPrice,
+            @NotNull @Valid String discountExpDate) {
         return null;
     }
 }
