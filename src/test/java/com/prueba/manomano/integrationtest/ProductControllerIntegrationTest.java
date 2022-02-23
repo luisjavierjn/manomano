444e4db575f60eb5fbfb8effd783688c613bdeb9
Luis Jimenez <luis.jimenez@nordstrom.com>
2022-02-22 21:04:50 -0500

    step 6 and 7 ready

--- src/test/java/com/prueba/manomano/integrationtest/ProductControllerIntegrationTest.java
+++ src/test/java/com/prueba/manomano/integrationtest/ProductControllerIntegrationTest.java
@@ -1,5 +1,46 @@
 package com.prueba.manomano.integrationtest;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerIntegrationTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductControllerIntegrationTest.class);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnBadRequest() throws Exception {
+        logger.info("Test shouldReturnBadRequest");
+        this.mockMvc.perform(get("/products/1/34")).andDo(print()).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldReturnNotFound() throws Exception {
+        logger.info("Test shouldReturnNotFound");
+        this.mockMvc.perform(get("/products/")).andDo(print()).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldReturnBadRequestBecauseDateTimeConversionFailed() throws Exception {
+        logger.info("Test shouldReturnBadRequestBecauseDateTimeConversionFailed");
+        this.mockMvc.perform(get("/products/1/34?discountExpDate=2021-10-16-00:00:00")).andDo(print()).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldReturnOk() throws Exception {
+        logger.info("Test shouldReturnOk");
+        this.mockMvc.perform(get("/products/1/50?discountExpDate=2021-04-14T00:00:00")).andDo(print()).andExpect(status().isOk());
+    }
 }
--- src/test/java/com/prueba/manomano/unittest/ProductControllerUnitTest.java
+++ src/test/java/com/prueba/manomano/unittest/ProductControllerUnitTest.java
@@ -1,22 +1,20 @@
 package com.prueba.manomano.unittest;
 
-
 import com.prueba.manomano.controller.ProductController;
+import com.prueba.manomano.model.ProductResponseList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductControllerUnitTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ProductControllerUnitTest.class);
@@ -25,7 +23,10 @@ class ProductControllerUnitTest {
     private ProductController productController;
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
 
     @Test
     public void contextLoads() {
@@ -33,26 +34,16 @@ class ProductControllerUnitTest {
     }
 
     @Test
-    public void shouldReturnBadRequest() throws Exception {
-        logger.info("Test shouldReturnBadRequest");
-        this.mockMvc.perform(get("/products/1/34")).andDo(print()).andExpect(status().isBadRequest());
+    public void controllerShouldReturnOk() throws Exception {
+        logger.info("Test controllerShouldReturnOk");
+        ResponseEntity<ProductResponseList> response = restTemplate.getForEntity("http://localhost:" + port + "/products/1/34/?discountExpDate=2021-10-16T00:00:00", ProductResponseList.class);
+        assertEquals(response.getStatusCode(),HttpStatus.OK);
     }
 
     @Test
-    public void shouldReturnNotFound() throws Exception {
-        logger.info("Test shouldReturnNotFound");
-        this.mockMvc.perform(get("/products/")).andDo(print()).andExpect(status().isNotFound());
-    }
-
-    @Test
-    public void shouldReturnOk() throws Exception {
-        logger.info("Test shouldReturnNotFound");
-        this.mockMvc.perform(get("/products/1/34?discountExpDate=2021-10-16T00:00:00")).andDo(print()).andExpect(status().isOk());
-    }
-
-    @Test
-    public void shouldReturnBadRequestBecauseDateTimeConversionFailed() throws Exception {
-        logger.info("Test shouldReturnNotFound");
-        this.mockMvc.perform(get("/products/1/34?discountExpDate=2021-10-16-00:00:00")).andDo(print()).andExpect(status().isBadRequest());
+    public void controllerShouldReturnBadRequest() throws Exception {
+        logger.info("Test controllerShouldReturnBadRequest");
+        ResponseEntity<ProductResponseList> response = restTemplate.getForEntity("http://localhost:" + port + "/products/1/34/?discountExpDate=", ProductResponseList.class);
+        assertEquals(response.getStatusCode(),HttpStatus.BAD_REQUEST);
     }
 }
